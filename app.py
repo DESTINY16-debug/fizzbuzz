36570abf210147d7f3013e7dbc1c524244eb8bcd
DESTINY16 de-bug <destiny.williams@kibo.school>
2023-10-16 03:18:21 -1100

    second commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def fizzbuzz():
+    numbers = []
+
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            numbers.append('FizzBuzz')
+        elif i % 3 == 0:
+            numbers.append('Fizz')
+        elif i % 5 == 0:
+            numbers.append('Buzz')
+        else:
+            numbers.append(i)
+
+    return render_template('fizzbuzz.html', numbers=numbers)
+
